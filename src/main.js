62fe74be55169b93bfbb8859b86132fe4466d1f8
JackpotMachine777 <jackpotmachinedev777@gmail.com>
2025-08-17 06:28:05 +0200

    Fixed uptime bug

--- src/main.js
+++ src/main.js
@@ -50,9 +50,9 @@ function formatTime(seconds){
   const mins = Math.floor((seconds % 3600) / 60);
   const secs = seconds % 60;
 
-  if (!mins) return `${secs}s`;
-  if (!hrs && mins) return `${mins}m ${secs}s`;
-  if (!days && hrs) return `${hrs}h ${mins}m ${secs}s`;
+  if (!days && !hrs && !mins) return `${secs}s`;
+  if (!days && !hrs) return `${mins}m ${secs}s`;
+  if (!days) return `${hrs}h ${mins}m ${secs}s`;
   else return `${days}d ${hrs}h ${mins}m ${secs}s`; 
 }
 
